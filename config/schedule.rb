f10d9803ed4ec5153ddc2588114b0edb7f468b87
roshan-webkorps <roshan@webkorps.com>
2025-08-22 03:31:40 -0400

    fix format

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,11 +1,11 @@
-env :PATH, ENV['PATH']
+env :PATH, ENV["PATH"]
 set :environment, ENV["RAILS_ENV"]
-set :output, 'log/cron.log'
+set :output, "log/cron.log"
 
 every 6.hours do
-  rake 'github:incremental_sync'
+  rake "github:incremental_sync"
 end
 
-every 6.hours, at: '0:30' do
-  rake 'jira:incremental_sync'
+every 6.hours, at: "0:30" do
+  rake "jira:incremental_sync"
 end
